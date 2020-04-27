43bd5c16b5fe6b9522f476aded7d495442dc65ef
Liudmila02 <vasulivna.02@gmail.com>
2020-04-27 21:55:14 +0300

    url

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const request = axios.create({
- baseURL: 'http://localhost:4000/',
+ baseURL: 'https://node-react-clients.herokuapp.com/',
  withCredentials: true,
 });
